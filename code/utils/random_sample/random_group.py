f4e3b86f82a2eee636e5c11dab481ad744879772
“Tara-Su“ <tarasulovescience@gmail.com>
2020-06-28 11:45:41 -0500

    modified to be more functional segmented.

--- code/utils/random_sample/random_group.py
+++ code/utils/random_sample/random_group.py
@@ -1,14 +1,33 @@
 import numpy as np
 
-group_list = [1, 2, 3]
-size = 1
-probability = [1, 1, 1]
-probability = np.divide(probability, sum(probability))
 
-choice = np.random.choice(group_list, size=size, replace=True, p=probability)
-print(" ")
-print("=start=" * 20)
-print("choice of the group:")
-print(choice)
-print("=end=" * 20)
-print(" ")
+def select_student_groups(group_list, size, probability):
+    probability = np.divide(probability, sum(probability))
+    choice = np.random.choice(group_list, size=size, replace=True, p=probability)
+    return choice
+
+
+def print_format(text):
+    print(" ")
+    print("=start=" * 20)
+    print(text)
+    print("=end=" * 20)
+    print(" ")
+
+if __name__ == "__main__":
+    # Set up
+    group_list = [1, 2, 3, 4]
+    size = 2
+    probability = [1] * len(group_list) # [1, 1, 1, 1]
+
+    # Select
+    choice = select_student_groups(group_list, size, probability)
+
+    # Output
+    text = """
+    choice of the group:
+    {0}
+    """.format(choice)
+    print_format(text)
+
+
